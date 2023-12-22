7fed39806501d87943f77cf5c859ecc53ee0885b
Victor Alvarez <victoralvarezsaucedo@gmail.com>
2023-12-22 17:06:59 -0600

    api return

--- src/app/api/product/update/route.js
+++ src/app/api/product/update/route.js
@@ -3,11 +3,11 @@ import { NextResponse } from "next/server";
 export async function POST(request, response) {
   console.log(request.body);
   console.log('POST')
-  return "ok";
+  return NextResponse.json("OK");
 }
 
 export async function PUT(request, response) {
   console.log(request.body);
   console.log('PUT')
-  return "ok";
+  return NextResponse.json("OK");
 }
