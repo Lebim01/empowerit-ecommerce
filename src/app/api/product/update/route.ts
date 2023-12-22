ff9ec9903ab41eff9296fbd9e0393e20ab7c166f
Victor Alvarez <victoralvarezsaucedo@gmail.com>
2023-12-22 17:30:16 -0600

    data

--- src/app/api/product/update/route.ts
+++ src/app/api/product/update/route.ts
@@ -1,12 +1,12 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(request: NextRequest) {
-  console.log(request);
-  console.log("POST");
+export async function POST(req: NextRequest) {
+  const data = await req.json();
+  console.log(data);
   return NextResponse.json("OK");
 }
 
-export async function PUT(request, response) {
+export async function PUT(request) {
   console.log(request.body);
   console.log("PUT");
   return NextResponse.json("OK");
