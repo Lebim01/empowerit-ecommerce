b816c7fb31b7eee815dbe80e34fd1264296e488a
Victor Alvarez <victoralvarezsaucedo@gmail.com>
2023-12-22 17:20:20 -0600

    fix api

--- src/app/api/product/delete/route.js
+++ src/app/api/product/delete/route.js
@@ -1,11 +1,13 @@
+import { NextResponse } from "next/server";
+
 export async function DELETE(request, response) {
   console.log(request.body);
   console.log('DELETE')
-  return "ok";
+  return NextResponse.json("OK");
 }
 
 export async function POST(request, response) {
   console.log(request.body);
   console.log('POST')
-  return "ok";
+  return NextResponse.json("OK");
 }
