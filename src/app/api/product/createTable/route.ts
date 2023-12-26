fb109e6cc84ebad7b1bdb25f81cd15481372cae1
Victor Alvarez <victoralvarezsaucedo@gmail.com>
2023-12-26 12:44:57 -0600

    fiox

--- src/app/api/product/createTable/route.ts
+++ src/app/api/product/createTable/route.ts
@@ -1,5 +1,7 @@
 import { createTable } from "@/postgresql/products";
+import { NextResponse } from "next/server";
 
-export const GET = () => {
-  return createTable();
+export const GET = async () => {
+  await createTable();
+  return NextResponse.json("OK");
 };
