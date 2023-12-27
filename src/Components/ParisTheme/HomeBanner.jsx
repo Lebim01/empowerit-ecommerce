7e9327ee1de9b1093b3cd6322c1f370ea10f9cf1
Victor Alvarez <victoralvarezsaucedo@gmail.com>
2023-12-27 11:55:22 -0600

    fix banner promo

--- src/Components/ParisTheme/HomeBanner.jsx
+++ src/Components/ParisTheme/HomeBanner.jsx
@@ -1,16 +1,27 @@
-import Slider from 'react-slick';
-import { themeSec2BannerSlider } from '../../../Data/SliderSettingsData';
-import WrapperComponent from '../Common/WrapperComponent';
-import OfferBanner from './OfferBanner';
+import Slider from "react-slick";
+import { themeSec2BannerSlider } from "../../../Data/SliderSettingsData";
+import WrapperComponent from "../Common/WrapperComponent";
+import OfferBanner from "./OfferBanner";
 
 const HomeBanner = ({ bannersData }) => {
-  const banners = bannersData.filter((elem) => (elem.status == true ? true : false));
+  const banners = bannersData.filter((elem) =>
+    elem.status == true ? true : false
+  );
+
   return (
-    <WrapperComponent classes={{ sectionClass: 'banner-section ratio_60' }} noRowCol={true}>
-      <div className='banner-slider'>
-        <Slider {...themeSec2BannerSlider}>
+    <WrapperComponent
+      classes={{ sectionClass: "banner-section ratio_60" }}
+      noRowCol={true}
+    >
+      <div className="banner-slider">
+        <Slider {...themeSec2BannerSlider} slidesToShow={banners.length}>
           {banners.map((elem, i) => (
-            <OfferBanner classes={{ customHoverClass: 'banner-contain hover-effect' }} imgUrl={elem?.image_url} key={i} elem={elem} />
+            <OfferBanner
+              classes={{ customHoverClass: "banner-contain hover-effect" }}
+              imgUrl={elem?.image_url}
+              key={i}
+              elem={elem}
+            />
           ))}
         </Slider>
       </div>
--- src/middleware.js
+++ src/middleware.js
@@ -70,5 +70,5 @@ export async function middleware(request) {
 }
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|assets).*)"],
 };
