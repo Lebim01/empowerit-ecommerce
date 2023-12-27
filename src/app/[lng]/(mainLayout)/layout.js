c4c2d0a7126d415d850bc89d792d9e00ba412dd7
Victor Alvarez <victoralvarezsaucedo@gmail.com>
2023-12-27 12:46:54 -0600

    google analytics

--- src/app/[lng]/(mainLayout)/layout.js
+++ src/app/[lng]/(mainLayout)/layout.js
@@ -1,8 +1,23 @@
 import MainLayout from "@/Layout";
+import Script from "next/script";
 
 export default function RootLayout({ children, params: { lng } }) {
   return (
     <>
+      {process.env.NODE_ENV == "production" && (
+        <>
+          <Script src="https://www.googletagmanager.com/gtag/js?id=G-WLQ8FSF7JE" />
+          <Script id="google-analytics">
+            {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', 'G-WLQ8FSF7JE');
+        `}
+          </Script>
+        </>
+      )}
       <MainLayout lng={lng}>{children}</MainLayout>
     </>
   );
