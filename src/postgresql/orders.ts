55740953c221d994d0fa70b0d1dd18fe0a796822
Victor Alvarez <victoralvarezsaucedo@gmail.com>
2024-01-16 11:18:05 -0600

    api cors

--- src/postgresql/orders.ts
+++ src/postgresql/orders.ts
@@ -7,6 +7,24 @@ export type Orders = {
   created_at?: Date;
 };
 
+const convertToPSQL = (processedProduct: Order) => ({
+  ...processedProduct,
+  can_review: processedProduct.can_review ? 1 : 0,
+  attributes: json(processedProduct.attributes),
+  product_galleries: json(processedProduct.product_galleries),
+  cross_sell_products: json(processedProduct.cross_sell_products),
+  variations: json(processedProduct.variations),
+  categories: json(processedProduct.categories),
+  tags: json(processedProduct.tags),
+  tax: processedProduct.tax ? json(processedProduct.tax) : null,
+  store: json(processedProduct.store),
+  related_products: json(processedProduct.related_products),
+  reviews: json(processedProduct.reviews),
+  product_meta_image: json(processedProduct.product_meta_image),
+  product_thumbnail: json(processedProduct.product_thumbnail),
+  review_ratings: json(processedProduct.review_ratings),
+});
+
 export const createTable = async () => {
   return dbClient.schema
     .createTable("orders")
