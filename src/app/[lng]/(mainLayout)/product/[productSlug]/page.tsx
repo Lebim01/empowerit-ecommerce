635245b42aede0d317178b5141470536ebbeaf99
Victor Alvarez <victoralvarezsaucedo@gmail.com>
2024-01-19 21:33:36 -0600

    keywords

--- src/app/[lng]/(mainLayout)/product/[productSlug]/page.tsx
+++ src/app/[lng]/(mainLayout)/product/[productSlug]/page.tsx
@@ -1,17 +1,27 @@
 import ProductDetailContent from "@/Components/ProductDetails";
+import { Metadata } from "next";
 
-export async function generateMetadata({ params }) {
+export async function generateMetadata({ params }): Promise<Metadata> {
   const productData = await fetch(
     `${process.env.API_PROD_URL}product/${params?.productSlug}`
   )
     .then((res) => res.json())
     .catch((err) => console.log("err", err));
 
-  console.log(productData);
+  console.log({
+    title: productData?.meta_title,
+    description: productData?.meta_description,
+    openGraph: {
+      title: productData?.meta_title,
+      description: productData?.meta_description,
+      images: [productData?.product_meta_image?.original_url, []],
+    },
+  });
 
   return {
     title: productData?.meta_title,
     description: productData?.meta_description,
+    keywords: productData?.tags?.join(', '),
     openGraph: {
       title: productData?.meta_title,
       description: productData?.meta_description,
