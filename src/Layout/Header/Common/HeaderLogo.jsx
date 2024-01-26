d849b66afb4ed256677c0394ce09a902d526b901
Victor Alvarez <victoralvarezsaucedo@gmail.com>
2024-01-25 18:00:40 -0600

    logo

--- src/Layout/Header/Common/HeaderLogo.jsx
+++ src/Layout/Header/Common/HeaderLogo.jsx
@@ -5,6 +5,7 @@ import ThemeOptionContext from "@/Helper/ThemeOptionsContext";
 import Avatar from "@/Components/Common/Avatar";
 import Link from "next/link";
 import { RiMenuLine } from "react-icons/ri";
+import Image from "next/image";
 
 const HeaderLogo = () => {
   const [logo] = useState("/assets/images/logo.png");
@@ -24,13 +25,7 @@ const HeaderLogo = () => {
         </span>
       </Btn>
       <Link href="/" className="web-logo nav-logo">
-        <Avatar
-          data={logo}
-          name={"Header"}
-          customImageClass={"img-fluid"}
-          height={28}
-          width={162}
-        />
+        <Image src={logo} width={500} height={300} quality={100} />
       </Link>
     </>
   );
