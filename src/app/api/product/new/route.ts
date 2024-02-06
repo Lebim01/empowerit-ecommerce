b224a4f668a079a1fce7a83f17f97ae77cd7d289
Victor Alvarez <victoralvarezsaucedo@gmail.com>
2024-02-05 18:27:10 -0600

    fix add

--- src/app/api/product/new/route.ts
+++ src/app/api/product/new/route.ts
@@ -1,5 +1,6 @@
 import { productGoogleCommerce } from "@/Utils/GoogleCommerce/etl";
 import { addProductCommerce } from "@/Utils/GoogleCommerce/products";
+import { getProductMetafieldsFromShopify } from "@/Utils/Shopify/products";
 import { productShopifyToStore } from "@/adapters/product";
 import { insertNewProduct } from "@/postgresql/products";
 import { ShopifyProduct } from "@/types/shopify";
@@ -8,7 +9,8 @@ import { NextRequest, NextResponse } from "next/server";
 export async function POST(req: NextRequest) {
   const data: ShopifyProduct = await req.json();
 
-  const processedProduct = productShopifyToStore(data);
+  const metafieldsProduct = await getProductMetafieldsFromShopify(data.admin_graphql_api_id);
+  const processedProduct = productShopifyToStore(data, metafieldsProduct);
   const googleProduct = productGoogleCommerce(processedProduct);
   //await addProductCommerce(googleProduct);
   await insertNewProduct(processedProduct);
