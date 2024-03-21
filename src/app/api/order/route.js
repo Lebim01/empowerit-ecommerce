784d809d0b6f88d2a3506e091f97ee513ea75c68
Victor Alvarez <victoralvarezsaucedo@gmail.com>
2024-03-21 17:02:36 -0600

    orders

--- src/app/api/order/route.js
+++ src/app/api/order/route.js
@@ -1,6 +1,5 @@
-import order from "./order.json";
 import { NextResponse } from "next/server";
 
 export async function GET(request) {
-  return NextResponse.json(order);
+  return NextResponse.json([]);
 }
