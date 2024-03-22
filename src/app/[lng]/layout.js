2806e18a85df5587421e44bfea4bf9b254598be4
Victor Alvarez <victoralvarezsaucedo@gmail.com>
2024-03-21 22:03:12 -0600

    fix gtag

--- src/app/[lng]/layout.js
+++ src/app/[lng]/layout.js
@@ -34,15 +34,18 @@ export default function CustomLayout({ children, params: { lng } }) {
           {process.env.NODE_ENV == "production" && (
             <>
               <script src="https://www.googletagmanager.com/gtag/js?id=G-WLQ8FSF7JE" />
-              <script id="google-analytics">
-                {`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-    
-              gtag('config', 'G-WLQ8FSF7JE');
-            `}
-              </script>
+              <script
+                id="google-analytics"
+                dangerouslySetInnerHTML={{
+                  __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+          
+                    gtag('config', 'G-WLQ8FSF7JE');
+                  `,
+                }}
+              />
             </>
           )}
           <link
